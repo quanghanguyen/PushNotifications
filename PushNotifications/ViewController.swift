3a72bde18d90ab06424e6952dbb7b6eab898b884
QuangHa <duongthien.mcs@gmail.com>
2022-10-03 14:48:57 +0700

    commit_1

--- PushNotifications/ViewController.swift
+++ PushNotifications/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        requestPermision()
+        addView()
     }
-
-
+    
+    private func requestPermision() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, err) in
+                    print("granted: (\(granted)")
+        }
+    }
+    
+    private func addView() {
+        addButton()
+    }
+    
+    func addButton() {
+        let button = UIButton(frame:CGRect(x: 0, y: 0, width: 200, height: 50))
+        button.center = view.center
+        button.configuration = createConfig()
+        button.addTarget(self, action: #selector(self.buttonClick), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    func createConfig() -> UIButton.Configuration {
+        var config: UIButton.Configuration = .filled()
+        config.baseBackgroundColor = .systemGreen
+        config.title = "Tap me"
+        config.titleAlignment = .center
+        return config
+    }
+    
+    @objc func buttonClick(sender: UIButton) {
+        print("Clicked")
+        let content = UNMutableNotificationContent()
+        content.title = "Notification"
+        content.body = "Look for the app ID you just created in Xcode"
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: "Quang-Ha.PushNotifications", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 }
 
